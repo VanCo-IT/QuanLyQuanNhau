eac371970f6221105f6732a2512a1e1218f5ab16
vanCo-IT <colv51614@gmail.com>
2025-10-17 13:27:17 +0700

    done code frmWelcon => frmLogin

--- GUI_QLQuanNhau/frmWelcom.cs
+++ GUI_QLQuanNhau/frmWelcom.cs
@@ -5,7 +5,20 @@ namespace GUI_QLQuanNhau
         public frmWelcom()
         {
             InitializeComponent();
-
+            progressBar.Style = ProgressBarStyle.Marquee;
+            progressBar.MarqueeAnimationSpeed = 30;
+            Task.Delay(3000).ContinueWith(t =>
+            {
+                if (this.IsHandleCreated && !this.IsDisposed)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        frmLogin login = new frmLogin();
+                        login.Show();
+                        this.Hide();
+                    }));
+                }
+            });
         }
     }
 }
