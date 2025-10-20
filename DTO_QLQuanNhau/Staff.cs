5773c574b961c56575b5a3c50c227b6ef19987f9
vanCo-IT <colv51614@gmail.com>
2025-10-20 17:45:39 +0700

    done code Staff/NhanVien

---
+++ DTO_QLQuanNhau/Staff.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_QLQuanNhau
+{
+    public class Staff
+    {
+        public sbyte MaNV { get; set; }
+        public string TenNV { get; set; }
+        public string SDT { get; set; }
+        public string TenDN { get; set; }
+        public string Email { get; set; }
+        public string MatKhau { get; set; }
+        public bool VaiTro { get; set; }
+        public bool TrangThai { get; set; }
+        public string VaiTroText => VaiTro ? "Quản lý" : "Nhân viên";
+        public string TrangThaiText => TrangThai ? "Đang làm việc" : "Đã nghỉ việc";
+    }
+}
