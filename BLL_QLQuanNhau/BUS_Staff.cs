a0060889eb8560c966769483c9700ccedf0983c8
vanCo-IT <colv51614@gmail.com>
2025-10-20 18:10:37 +0700

    create and done code BUS_Staff

---
+++ BLL_QLQuanNhau/BUS_Staff.cs
@@ -0,0 +1,116 @@
+﻿using DAL_QLQuanNhau;
+using DTO_QLQuanNhau;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_QLQuanNhau
+{
+    public class BUS_Staff
+    {
+        DAL_Staff dalStaff = new DAL_Staff();
+
+        public Staff DangNhap(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+
+            return dalStaff.getStaff(username, password);
+        }
+
+        public bool ResetMatKhau(string email, string mk)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(mk))
+                {
+                    return false;
+                }
+                dalStaff.ResetMatKhau(mk, email);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public List<Staff> GetNhanVienList()
+        {
+            return dalStaff.selectAll();
+        }
+
+
+        public string InsertNhanVien(Staff nv)
+        {
+            try
+            {
+                nv.MaNV = dalStaff.generateMaNhanVien();
+                if (string.IsNullOrEmpty(nv.MaNV))
+                {
+                    return "Mã nhân viên không hợp lệ.";
+                }
+                if (dalStaff.checkEmailExists(nv.Email))
+                {
+                    return "Email đã tồn tại.";
+                }
+                dalStaff.insertNhanVien(nv);
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                //return "Thêm mới không thành công.";
+                return "Lỗi: " + ex.Message;
+            }
+        }
+
+        public string UpdateNhanVien(Staff nv)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(nv.MaNV))
+                {
+                    return "Mã nhân viên không hợp lệ.";
+                }
+
+                dalStaff.updateNhanVien(nv);
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                //return "Cập nhật không thành công.";
+                return "Lỗi: " + ex.Message;
+            }
+        }
+
+        public string DeleteNhanVien(string maNV)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(maNV))
+                {
+                    return "Mã nhân viên không hợp lệ.";
+                }
+
+                dalStaff.deleteNhanVien(maNV);
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                //return "Xóa không thành công.";
+                return "Lỗi: " + ex.Message;
+            }
+        }
+        public bool KiemTraEmailTonTai(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            return dalStaff.checkEmailExists(email);
+        }
+    }
+}
