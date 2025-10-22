4f48348cfabebb662de424a1b897b47763c7f5ce
vanCo-IT <colv51614@gmail.com>
2025-10-22 12:24:56 +0700

    change UI frmLogin

--- GUI_QLQuanNhau/frmLogin.cs
+++ GUI_QLQuanNhau/frmLogin.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL_QLQuanNhau;
+using DTO_QLQuanNhau;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UTIL_QLQuanNhau;
 
 namespace GUI_QLQuanNhau
 {
     public partial class frmLogin : Form
     {
+        BUS_Staff bUS_Staff = new BUS_Staff();
         public frmLogin()
         {
             InitializeComponent();
+        }
+
+        private void frmLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void cbHienMK_CheckedChanged(object sender, EventArgs e)
+        {
+            txtMatKhau.PasswordChar = cbHienMK.Checked ? '\0' : '*';
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string username = txtEmail.Text;
+            string password = txtMatKhau.Text;
+            Staff nv = bUS_Staff.DangNhap(username, password);
+            if (nv == null)
+            {
+                MessageBox.Show(this, "Tài khoản hoặc mật khẩu không chính xác");
+            }
+            else
+            {
+                if (nv.TrangThai == false)
+                {
+                    MessageBox.Show(this, "Tài khoản đang tạm khóa, vui lòng viên hệ QTV!!!");
+                    return;
+                }
+                AuthUtil.user = nv;
+
+                frmMain main = new frmMain();
+                main.Show();
+                this.Hide();
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn muốn thoát khỏi chương trình", "Thoát",
+                           MessageBoxButtons.YesNo,
+                           MessageBoxIcon.Question
+                           );
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
         }
     }
 }
