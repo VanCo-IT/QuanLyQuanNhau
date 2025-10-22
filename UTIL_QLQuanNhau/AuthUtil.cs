c60f493a3f80ea19951b441faebc6f41e511a47e
vanCo-IT <colv51614@gmail.com>
2025-10-22 12:16:25 +0700

    add class user to UTIL_QLQuanNhau/AuthUtil.cs

--- UTIL_QLQuanNhau/AuthUtil.cs
+++ UTIL_QLQuanNhau/AuthUtil.cs
@@ -1,7 +1,21 @@
-﻿namespace UTIL_QLQuanNhau
+﻿using DTO_QLQuanNhau;
+
+namespace UTIL_QLQuanNhau
 {
     public class AuthUtil
     {
-
+        public static Staff user = null;
+        public static bool IsLogin()
+        {
+            if (user == null)
+            {
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(user.MaNV))
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
