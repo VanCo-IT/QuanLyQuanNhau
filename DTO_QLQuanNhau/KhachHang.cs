c52967fa707c22212af07ac8440b5341a4550168
vanCo-IT <colv51614@gmail.com>
2025-10-31 16:22:12 +0700

    create dto/KhachHang

---
+++ DTO_QLQuanNhau/KhachHang.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_QLQuanNhau
+{
+    public class KhachHang
+    {
+        public string MaKH { get; set; }
+        public string TenKH { get; set; }
+        public string SDT { get; set; }
+        public string Email { get; set; }
+        public string DiaChi { get; set; }
+        public string LoaiKH { get; set; }
+        public string MoTa { get; set; }
+
+    }
+}
