d96bd57f85e79089591af309e5c82cfe15c335df
vanCo-IT <colv51614@gmail.com>
2025-11-19 15:20:54 +0700

    add picture kho

--- GUI_QLQuanNhau/frmKho.Designer.cs
+++ GUI_QLQuanNhau/frmKho.Designer.cs
@@ -28,18 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmKho));
+            pnThongTin = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            pnThongTin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // pnThongTin
+            // 
+            pnThongTin.Controls.Add(label1);
+            pnThongTin.Controls.Add(pictureBox1);
+            pnThongTin.Dock = DockStyle.Top;
+            pnThongTin.Location = new Point(0, 0);
+            pnThongTin.Name = "pnThongTin";
+            pnThongTin.Size = new Size(884, 100);
+            pnThongTin.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(106, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(48, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Mã Kho";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Red;
+            pictureBox1.Dock = DockStyle.Left;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 100);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
             // frmKho
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(884, 561);
+            Controls.Add(pnThongTin);
             Name = "frmKho";
             Text = "frmKho";
+            pnThongTin.ResumeLayout(false);
+            pnThongTin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel pnThongTin;
+        private PictureBox pictureBox1;
+        private Label label1;
     }
 }
