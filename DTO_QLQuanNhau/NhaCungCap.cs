41e7342fec6b16eb0776a00cf6c810c60e57226e
vanCo-IT <colv51614@gmail.com>
2025-11-20 20:55:05 +0700

    done code DTO/NhaCungCap

---
+++ DTO_QLQuanNhau/NhaCungCap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_QLQuanNhau
+{
+    public class NhaCungCap
+    {
+        public static string MaCungCap { get; set; }
+        public static string TenNCC { get; set; }
+        public static string SDT {get;set;}
+        public static string Email { get; set; }
+        public static string DicChi { get; set; }
+    }
+}
