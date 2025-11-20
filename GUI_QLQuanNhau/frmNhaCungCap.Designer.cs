b0a912ac91c3ec202f9d898bf1d6836696f1459e
vanCo-IT <colv51614@gmail.com>
2025-11-20 15:06:41 +0700

    create frmNhaCungCap

---
+++ GUI_QLQuanNhau/frmNhaCungCap.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace GUI_QLQuanNhau
+{
+    partial class frmNhaCungCap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // frmNhaCungCap
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "frmNhaCungCap";
+            Text = "Nhà cung cấp";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
