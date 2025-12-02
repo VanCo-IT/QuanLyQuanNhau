c73e9fa2c9d7742a9013bb7afc90269ede9e136b
vanCo-IT <colv51614@gmail.com>
2025-12-02 13:32:51 +0700

    update VS 2026 and add BUS_NhaCungCap

--- BLL_QLQuanNhau/BUS_NhaCungCap.cs
+++ BLL_QLQuanNhau/BUS_NhaCungCap.cs
@@ -1,12 +1,79 @@
-﻿using System;
+﻿using DAL_QLQuanNhau;
+using DTO_QLQuanNhau;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BLL_QLQuanNhau
 {
-    internal class BUS_NhaCungCap
+    public class BUS_NhaCungCap
     {
+        DAL_NhaCungCap dalNhaCungCap = new DAL_NhaCungCap();
+        public List<NhaCungCap> GetNhaCungCapList()
+        {
+            return dalNhaCungCap.selectAll();
+        }
+
+        public string InsertNhaCungCap(NhaCungCap ncc)
+        {
+            try
+            {
+                ncc.MaCungCap = dalNhaCungCap.generateMaCungCap();
+                if (string.IsNullOrEmpty(ncc.MaCungCap))
+                {
+                    return "Mã cung cấp không hợp lệ";
+        
+                }
+                dalNhaCungCap.insertNhaCungCap(ncc);
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                return "Lỗi: " + ex.Message;
+            }
+        }
+
+        public string UpdatNhaCUngCap(NhaCungCap ncc)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(ncc.MaCungCap))
+                {
+                    return "Mã cung cấp không hợp lệ";
+        
+        }
+                dalNhaCungCap.updateNhaCungCap(ncc);
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                return "Lỗi: " + ex.Message;
+            }
+        }
+
+        public string DeleteNhaCungCap(string maCungCap)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(maCungCap))
+                {
+                    return "Mã cung cấp không hợp lệ";
+        
+        }
+                dalNhaCungCap.deleteNhaCungCap(maCungCap);
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                return "Lỗi: " + ex.Message;
+            }
+        }
+        //public List<string> GetAllMaKH()
+        //{
+        //    return dalNhaCungCap.GetOnlyMaNCC();
+        //}
     }
 }
